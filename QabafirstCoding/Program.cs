76c60214a9111f8661ee231e3ca0f63332474ffb
Qababa1000 <deltagare@LEXJONKAF07>
2020-09-10 09:37:00 +0200

    Add project files.

---
+++ QabafirstCoding/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+
+namespace QabafirstCoding
+{
+    /*
+     
+    block comment
+
+    */
+
+    // inline comments
+
+    // Alt key + arrow key up/down 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, I like to code!");
+
+            Console.ReadKey();
+
+        }
+
+
+
+    }
+
+}
+
+
+
